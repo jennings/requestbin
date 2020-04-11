f3c8dd4cbadc4f54a38fbf859696cba568fb3f6b
Stephen Jennings <stephen.g.jennings@gmail.com>
2020-04-10 23:11:20 -0700

    use colon as Redis prefix separator

--- requestbin/storage/redis.py
+++ requestbin/storage/redis.py
@@ -1,9 +1,5 @@
-from __future__ import absolute_import
-
-import time
-import cPickle as pickle
-
 import redis
+import time
 
 from ..models import Bin
 
@@ -16,22 +12,22 @@ class RedisStorage():
         self.bin_ttl = bin_ttl
         self.redis = redis.StrictRedis(host=config.REDIS_HOST, port=config.REDIS_PORT, db=config.REDIS_DB, password=config.REDIS_PASSWORD)
 
-    def _key(self, name):
-        return '{}_{}'.format(self.prefix, name)
+    def _bin_key(self, name):
+        return '{}:bins:{}'.format(self.prefix, name)
 
     def _request_count_key(self):
-        return '{}-requests'.format(self.prefix)
+        return '{}:request_count'.format(self.prefix)
 
     def create_bin(self, private=False):
         bin = Bin(private)
-        key = self._key(bin.name)
+        key = self._bin_key(bin.name)
         self.redis.set(key, bin.dump())
         self.redis.expireat(key, int(bin.created+self.bin_ttl))
         return bin
 
     def create_request(self, bin, request):
         bin.add(request)
-        key = self._key(bin.name)
+        key = self._bin_key(bin.name)
         self.redis.set(key, bin.dump())
         self.redis.expireat(key, int(bin.created+self.bin_ttl))
 
@@ -39,7 +35,7 @@ class RedisStorage():
         self.redis.incr(self._request_count_key())
 
     def count_bins(self):
-        keys = self.redis.keys("{}_*".format(self.prefix))
+        keys = self.redis.keys(self._bin_key("*"))
         return len(keys)
 
     def count_requests(self):
@@ -50,7 +46,7 @@ class RedisStorage():
         return info['used_memory'] / info['db0']['keys'] / 1024
 
     def lookup_bin(self, name):
-        key = self._key(name)
+        key = self._bin_key(name)
         serialized_bin = self.redis.get(key)
         try:
             bin = Bin.load(serialized_bin)
