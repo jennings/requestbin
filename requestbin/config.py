cce6b7d3c44f2156b0ae43becde524b3e2f6fa63
Stephen Jennings <stephen.g.jennings@gmail.com>
2020-04-10 23:07:34 -0700

    remove STORAGE_BACKEND env var
    The existence of REDIS_URL switches to using Redis automatically.

--- requestbin/config.py
+++ requestbin/config.py
@@ -1,52 +1,35 @@
 import os
 from urllib.parse import urlparse
 
-DEBUG = True
-REALM = os.environ.get('REALM', 'local')
-
-ROOT_URL = "http://localhost:4000"
-
 PORT_NUMBER = 4000
 
 ENABLE_CORS = False
 CORS_ORIGINS = "*"
 
-FLASK_SESSION_SECRET_KEY = os.environ.get("SESSION_SECRET_KEY", "N1BKhJLnBqLpexOZdklsfDKFJDKFadsfs9a3r324YB7B73AglRmrHMDQ9RhXz35")
-
 BIN_TTL = 48*3600
-STORAGE_BACKEND = "requestbin.storage.memory.MemoryStorage"
 MAX_RAW_SIZE = int(os.environ.get('MAX_RAW_SIZE', 1024*10))
-IGNORE_HEADERS = []
 MAX_REQUESTS = 20
 CLEANUP_INTERVAL = 3600
 
-REDIS_URL = ""
-REDIS_HOST = "localhost"
-REDIS_PORT = 6379
-REDIS_PASSWORD = None
-REDIS_DB = 9
+STORAGE_BACKEND = "requestbin.storage.memory.MemoryStorage"
+REDIS_URL = os.environ.get("REDIS_URL")
+if REDIS_URL is not None:
+    STORAGE_BACKEND = "requestbin.storage.redis.RedisStorage"
+    _url_parts = urlparse(REDIS_URL)
+    REDIS_HOST = _url_parts.hostname
+    REDIS_PORT = _url_parts.port
+    REDIS_PASSWORD = _url_parts.password
+    REDIS_DB = _url_parts.fragment
+    REDIS_PREFIX = "requestbin"
 
-REDIS_PREFIX = "requestbin"
 
-BUGSNAG_KEY = ""
+REALM = os.environ.get('REALM', 'local')
 
 if REALM == 'prod':
     DEBUG = False
     ROOT_URL = "http://requestb.in"
-
     FLASK_SESSION_SECRET_KEY = os.environ.get("SESSION_SECRET_KEY", FLASK_SESSION_SECRET_KEY)
-
-    STORAGE_BACKEND = "requestbin.storage.redis.RedisStorage"
-
-    REDIS_URL = os.environ.get("REDIS_URL")
-    url_parts = urlparse(REDIS_URL)
-    REDIS_HOST = url_parts.hostname
-    REDIS_PORT = url_parts.port
-    REDIS_PASSWORD = url_parts.password
-    REDIS_DB = url_parts.fragment
-
     BUGSNAG_KEY = os.environ.get("BUGSNAG_KEY", BUGSNAG_KEY)
-
     IGNORE_HEADERS = """
 X-Varnish
 X-Forwarded-For
@@ -59,3 +42,10 @@ X-Forwarded-Proto
 X-Via
 X-Forwarded-Port
 """.split("\n")[1:-1]
+
+else:
+    DEBUG = True
+    ROOT_URL = "http://localhost:4000"
+    FLASK_SESSION_SECRET_KEY = os.environ.get("SESSION_SECRET_KEY", "N1BKhJLnBqLpexOZdklsfDKFJDKFadsfs9a3r324YB7B73AglRmrHMDQ9RhXz35")
+    BUGSNAG_KEY = ""
+    IGNORE_HEADERS = []
