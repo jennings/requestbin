5f007aab30e16d925cc83eb3617d105eae3020fe
Stephen Jennings <stephen.g.jennings@gmail.com>
2022-04-08 12:11:10 -0700

    remove Sponsored-By header

--- requestbin/views.py
+++ requestbin/views.py
@@ -47,7 +47,6 @@ def bin(name):
     else:
         db.create_request(bin, request)
         resp = make_response("ok\n")
-        resp.headers['Sponsored-By'] = "https://www.runscope.com"
         return resp
 
 
